6b6c3ea41daf0ed8b52f6ff2b3cf9a75e20f94d8
Botan Elveren <botanelveren@gmail.com>
2025-01-08 17:37:33 +0300

    Add project files.

---
+++ Encapsulation/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        private int _numberOfDoors;
+        public int NumberOfDoors
+        {
+            get
+            {
+                return _numberOfDoors;
+            }
+
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+                    _numberOfDoors = value;
+                }
+                else
+                {
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olabilir.");
+                    _numberOfDoors = -1;
+                }
+            }
+        }
+
+        public Car(string brand, string model, string color, int numberOfDoors)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            NumberOfDoors = numberOfDoors;
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,7 @@
+﻿using Encapsulation;
+
+Car car1 = new Car("Ford", "Impala", "Black", 4);
+Console.WriteLine($"Kapı sayısı: {car1.NumberOfDoors}");
+
+Car car2 = new Car("Chevrolet", "Camaro", "Red", 3);
+Console.WriteLine(car2.NumberOfDoors);
